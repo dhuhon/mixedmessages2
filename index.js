2a1e5d686acf765ae8b2ff037154dba9bae0b4e5
Solly <dhuhon@gmail.com>
2022-09-11 13:25:28 -0500

    message components

---
+++ index.js
@@ -0,0 +1,15 @@
+//Dad Jokes
+
+var dadJokes =["I'm afraid for the calendar. Its days are numbered.","I thought the dryer was shrinking my clothes. Turns out it was the refrigerator all along","Dear Math, grow up and solve your own problems"];
+var astrology =["Libra: You will be full of joy, enthusiasum and vigor today","Capricorn: Your grand orchestration may not come to fruition, and even your best-laid plans may go awry","Gemini: You will be busy organising some religious function today or in visiting a place of religious significance"];
+var netflixMovies =["Cobra Kai","The Witcher","Stranger Things"];
+const storedMessages = (dadJokes,astrology,netflixMovies)=> {
+return{
+    dadJokes,
+    astrology,
+    netflixMovies
+
+}
+
+
+}
