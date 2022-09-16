865e8078b00d7fd54a45ea2a48f6a4a85766ff57
Solly <dhuhon@gmail.com>
2022-09-15 19:45:16 -0500

    final code

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ var astrology =["Libra: You will be full of joy, enthusiasum and vigor today","C
 var netflixMovies =["Cobra Kai","The Witcher","Stranger Things"];
 const storedMessages = (dadJokes,astrology,netflixMovies)=> {
 return{
-    dadJokes,
-    astrology,
-    netflixMovies
+    dadJokes: dadJokes,
+    astrology: astrology,
+    netflixMovies: netflixMovies
 
 }
 
 
 }
+var random = Math.floor(Math.random()* 3);
+//console.log(random);
+var msg = storedMessages(dadJokes,astrology,netflixMovies);
+//console.log(msg.dadJokes[2]);
+var msg1,msg2,msg3;
+
+
+
+var dadjkesmsg =  'Dad Joke: ' + msg.dadJokes[Math.floor(Math.random() * msg.dadJokes.length)];
+var astrologymsg = '. Astrology Horoscope: ' + msg.astrology[Math.floor(Math.random() * msg.astrology.length)];
+var netflixMoviesmsg = '. Best Show on Netflix: '+ msg.netflixMovies[Math.floor(Math.random() * msg.netflixMovies.length)];
+console.log(dadjkesmsg,astrologymsg,netflixMoviesmsg);
+
